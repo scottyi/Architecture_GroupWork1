72b787ea8b0e4dc3f2e1d03927699a7bb9b4bc40
Scott Ivinza <Scott@MacBook-Pro-de-Scott.local>
2015-10-17 15:12:17 +0200

    Implementation et test de IOFile

--- IOFile.java
+++ IOFile.java
@@ -1,7 +1,61 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /***************
  * classe IOFile
  ***************/
 
 public class IOFile {
-
+	
+	// attribut de IOFile
+	private BufferedReader reader;
+	private String line; // line répresente une ligne courante du fichier
+	
+	//constructeur de IOFile
+	public IOFile(String in){
+		try{
+			reader = new BufferedReader(new FileReader(in));
+		}catch(FileNotFoundException e){
+			System.err.println("Unable to open file : " + in);	
+			e.printStackTrace();
+		}
+	}
+	
+	/* Cette méthode lit une ligne du fichier
+	 * 
+	 */
+	public String readNextLine(){
+		String s=null;
+		// Lecture d'une ligne du fichier
+		try{
+			s=reader.readLine();
+		}catch (IOException e){
+			System.err.println("Unable to read next line in input file");
+			e.printStackTrace();
+		} finally{
+		// Si le fichier est terminé ç-à-d que reader.readLine()==null
+			if(s==null){
+				line = null;
+			}
+			else{
+				line = s;
+			}
+		}
+		return line;
+	}
+	
+	/* Cette méthode ferme le flux (stream) et libère toutes les ressources
+	 * du système qui lui ont été associées.
+	 */
+	public void Close(){
+		try{
+			reader.close();
+		}catch(IOException e){
+			System.err.println("Termination failed");
+			e.printStackTrace();
+		}
+		
+	}
 }
--- Main.java
+++ Main.java
@@ -1,12 +1,24 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /*************
  * classe Main
  *************/
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		
+		// Test de la classe IOFile(ça fonctionne)
+		IOFile io = new IOFile("src/trace.txt");
+		String line = io.readNextLine();
+		while(line!=null){
+			System.out.println(line);
+			line = io.readNextLine();
+		}
+		io.Close();
+		
 	}
-
 }
